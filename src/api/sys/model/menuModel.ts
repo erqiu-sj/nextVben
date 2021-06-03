e5ebedb670be42b4a5e86096748368a348bdf852
lcedRedTea <58790073+erqiu-sj@users.noreply.github.com>
2021-06-03 10:07:03 +0800

    Initial commit

---
+++ src/api/sys/model/menuModel.ts
@@ -0,0 +1,16 @@
+import { RouteMeta } from '/@/router/types';
+export interface RouteItem {
+  path: string;
+  component: any;
+  meta: RouteMeta;
+  name?: string;
+  alias?: string | string[];
+  redirect?: string;
+  caseSensitive?: boolean;
+  children?: RouteItem[];
+}
+
+/**
+ * @description: Get menu return value
+ */
+export type getMenuListResultModel = RouteItem[];
